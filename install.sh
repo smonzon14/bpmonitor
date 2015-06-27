d33c1b4cb18b8676707aefac54aaeb4c278227c6
smonzon14 <smonzon360@gmail.com>
2015-06-27 13:12:35 -0400

    second commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+printf "Install ${RED}BPMonitor${NC} version 0.0.0?"
+echo ""
+read input
+if [ "$input" == y ]; then
+cd ~/bpmonitor
+chmod 777 bpm
+sudo rm //usr/bin/bpm
+sudo cp bpm /usr/bin
+echo '-------------------------------'
+echo 'make sure to apt-get install python-numpy python-psutil'
+echo ''
+echo 'bpmonitor successfully installed'
+exit 0
+else
+echo 'quit'
+exit 1
+fi
